de3a6932401c784d387167c11d18bf85db3bad9d
Hurduz Oksana <hurduzoksana@gmail.com>
2022-11-17 21:48:17 +0200

    Adding second task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
 
+import localStorageApi from './localstorage';
+
+// Робимо доступ до форми
+const formContactElement = document.querySelector(`.feedback-form`);
+
+const userData = {};
+
+const formFill = () => {
+    const dataFromLocalStorage = localStorageApi.load(`feedback-form-state`);
+    // Перевірка: якщо undefined, то нічого не робимо
+    if (dataFromLocalStorage === undefined) {
+        return;
+    }
+    const formEl = formContactElement;
+    for (const key in dataFromLocalStorage) {
+        if (dataFromLocalStorage.hasOwnproperty(key)) {
+            formEl[key].value = dataFromLocalStorage[key];
+        }
+    }
+};
+
+// Описуємо функцію-обробник
+
+onFormElementChange = event => {
+    // Виносимо цільовий елемент
+    const target = event.target;
+    // Доступ до значення елемента
+    const formElementValue = target.value;
+    // Доступ до імені елемента
+    const formElementName = target.name;
+
+    userData[formElementName] = formElementValue;
+    // Зберігаємо дані в local storage
+    localStorageApi.save(`feedback-form-state`, userData);
+};
+
+const onFormSubmit = event => {
+    // Запобігаємо перезавантаженню
+    event.preventDefault();
+    localStorageApi.remove(`feedback-form-state`);
+    // Чистимо local storage від даних після сабміта
+    event.currentTarget.reset();
+}
+
+
+formContactElement.addEventListener(`change`, onFormElementChange);
+formContactElement.addEventListener(`submit`, onFormSubmit);
+
+formFill();
