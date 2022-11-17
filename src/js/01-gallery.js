357ba45406db6154ea29bed40b0e96b2175e2456
Hurduz Oksana <hurduzoksana@gmail.com>
2022-11-17 12:09:37 +0200

    Adding first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,33 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Створюємо посилання на елемент
+const galleryList = document.querySelector(`.gallery`);
+
+// Створюємо елемент за допомогою шаблонного рядка
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+
+  return `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `
+};
+
+
+
+// Створюємо масив елементів
+const galleryCardContainer = galleryItems.map((el, idx, arr) => {
+  return makeGalleryCard(el);
+});
+
+// Додаємо розмітку в html
+galleryList.insertAdjacentHTML(`afterbegin`, galleryCardContainer.join(``));
+
+new SimpleLightbox('.gallery a', { 
+  captionsData: `alt`,
+  captionDelay: `250`
+ });
