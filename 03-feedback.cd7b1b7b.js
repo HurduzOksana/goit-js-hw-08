8ff43bc799781c21fb5936d1e04b43dbcc32ebb8
HurduzOksana <112498900+HurduzOksana@users.noreply.github.com>
2022-11-21 12:29:54 +0000

    Deploying to gh-pages from @ HurduzOksana/goit-js-hw-08@f14fbe4193d62566a8943dfb1465b21144f56a49 🚀

---
+++ 03-feedback.cd7b1b7b.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".js-feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){e.currentTarget.value}));
+//# sourceMappingURL=03-feedback.cd7b1b7b.js.map
